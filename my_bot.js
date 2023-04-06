be788d0f9cc8bb2c8db4021ea0b810ce172e80b4
Jerome Wong <jeromewongjh@gmail.com>
2023-04-06 11:17:08 +0800

    feat: implement menu

--- my_bot.js
+++ my_bot.js
@@ -1,21 +1,31 @@
 // Import the Grammy library
 import { Bot } from "grammy";
+import { Menu } from "@grammyjs/menu";
+
 import dotenv from "dotenv";
-// Replace YOUR_BOT_TOKEN with the token you got from BotFather
 dotenv.config();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const bot = new Bot(BOT_TOKEN);
-// Add a command handler for the /start command
-bot.command("start", (ctx) => {
-  ctx.reply(
-    "Welcome to my bot! Send me a message, and I'll echo it back to you."
-  );
-});
 
-// Add a message handler
+// Create a simple menu.
+const menu = new Menu("my-menu-identifier")
+  .text("A", (ctx) => ctx.reply("You pressed A!"))
+  .row()
+  .text("B", (ctx) => ctx.reply("You pressed B!"));
+
+// Make it interactive.
+bot.use(menu);
+
+bot.command("start", async (ctx) => {
+  // ctx.reply(
+  //   "Welcome to my bot! Send me a message, and I'll echo it back to you."
+  // );
+  await ctx.reply("Check out this menu:", { reply_markup: menu });
+});
+bot.setMyCommands;
+
 bot.on("message", (ctx) => {
   ctx.reply(`You said: ${ctx.message.text}`);
 });
 
-// Start the bot
 bot.start();
